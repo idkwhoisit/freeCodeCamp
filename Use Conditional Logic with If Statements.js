e5a3d8f95420afa5ca6fa8f70c46a567d64b0194
idkwhoisit <61099534+idkwhoisit@users.noreply.github.com>
2023-12-02 17:29:40 +0300

    Add files via upload

---
+++ Use Conditional Logic with If Statements.js
@@ -0,0 +1,15 @@
+// Create an if statement inside the function to return Yes, that was true if the parameter wasThatTrue is true and return No, that was false otherwise.
+
+//  My solution
+
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+
+
+    // Only change code above this line
+
+}
